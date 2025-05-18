440294c76dad7cd01dd81255397bab9d9a328cb2
Bitsmith16 <mail.sandeep.anand@gmail.com>
2025-05-18 11:18:04 +0200

    Model Hypertuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,object):
@@ -19,12 +20,20 @@ def save_object(file_path,object):
     except Exception as e:
             raise CustomException(e,sys)
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
         for i in range(len(list(models))):  # Going though Each model
             model = list(models.values())[i]
-            model.fit(X_train,y_train) # Training model
+            param = param[list(param.keys())[i]]
+
+            gs = GridSearchCV(model,param,cv=3)  # Get the best parameter for each model
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)   # Training model with best parameter
+
+            #model.fit(X_train,y_train) # Training model
 
             y_train_pred = model.predict(X_train)  # Predicting y_train value for provided x_train
             y_test_pred = model.predict(X_test)    # Predicting y_test value for provided x_test
